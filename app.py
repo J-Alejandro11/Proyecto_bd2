90cd68a4794ef656937a21d2a57d97662eb039c5
Alejandro Figueroa <jfigueroatz@gmail.com>
2025-05-20 05:25:46 -0600

    :beers: Portada Del Programa

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # ¡No necesita template_folder si usas la estructura estándar!
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Flask automáticamente buscará en templates/
+
+# Rutas para los otros templates
+@app.route('/estudiantes')
+def estudiantes():
+    return render_template('estudiantes.html')
+
+@app.route('/docentes')
+def docentes():
+    return render_template('docentes.html')
+
+@app.route('/planes')
+def planes():
+    return render_template('planes.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
